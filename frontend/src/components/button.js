5a155ad4c1666af2623b248b0ea6fec31212b788
jodi <128635337+jodi-j@users.noreply.github.com>
2024-12-05 23:24:46 -0500

    Login Page Code

---
+++ frontend/src/components/button.js
@@ -0,0 +1,56 @@
+import { fontWeight } from '@mui/system';
+import React from 'react';
+
+function Button({
+  label,
+  onClick,
+  type = 'button',
+  style = {},
+  disabled = false,
+  variant = 'primary',
+}) {
+  const baseStyle = {
+    margin: '10px',
+    padding: '10px 20px',
+    width: '450px',
+    fontFamily: 'Poppins, sans-serif',
+    fontSize: '16px',
+    fontWeight: '800',
+    color: 'white',
+    border: 'none',
+    borderRadius: '20px',
+    cursor: disabled ? 'not-allowed' : 'pointer',
+    transition: 'background-color 0.3s',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)',
+    ...style,
+  };
+
+  const variantStyles = {
+    primary: {
+      backgroundColor: '#9D6E49',
+      hover: '#724624',
+    },
+    accent: {
+      backgroundColor: '#C49D81',
+      hover: '#C6936E',
+    },
+  };
+
+  const currentVariantStyle = variantStyles[variant];
+  const buttonStyle = { ...baseStyle, backgroundColor: currentVariantStyle.backgroundColor };
+
+  return (
+    <button
+      type={type}
+      style={buttonStyle}
+      onClick={onClick}
+      disabled={disabled}
+      onMouseOver={(e) => (e.target.style.backgroundColor = currentVariantStyle.hover)}
+      onMouseOut={(e) => (e.target.style.backgroundColor = currentVariantStyle.backgroundColor)}
+    >
+      {label}
+    </button>
+  );
+}
+
+export default Button;
--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -1,9 +1,77 @@
-import React from 'react';
+import React, {useState} from "react";
+import "./login.css";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Button from "../../components/button";
+import LockIcon from '@mui/icons-material/Lock';
+import PersonIcon from '@mui/icons-material/Person';
 
 const Login = () => {
-    return (
-        <h1>Login Page</h1>
-    );
+  const navigate = useNavigate();
+  
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5001/api/auth/login', {username, password});
+
+      localStorage.setItem('session_id', response.data.session_id);
+      alert(response.data.message);
+    } catch(err){
+      console.error('Login error:', err);
+      alert("An error occurred"); 
+    }
+  }
+
+  return (
+    <div className="login-container">
+      <h1 className="title">Bright Scholarship</h1>
+      <div className="login-box">
+        <h2 className="welcome-text">Welcome back, Scholar!</h2>
+        <p className="subtext">Please log in to enter your scholarship portal.</p>
+
+        <form onSubmit={handleLogin}>
+
+          <div className="input-group">
+            <label htmlFor="username">
+              <i className="username-icon"></i>
+            </label>
+            <PersonIcon sx={{ fontSize: 40, color: '#9D6E49' }} />
+            <input type="text" id="username" className="username-input" placeholder="Username" value={username} 
+            onChange={(e) => setUsername(e.target.value)}/>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">
+              <i className="password-icon"></i>
+            </label>
+            <LockIcon sx={{ fontSize: 40, color: '#9D6E49' }}/>
+            <input type="password" id="password" className="password-input" placeholder="Password" value={password} 
+            onChange={(e) => setPassword(e.target.value)}/>
+          </div>
+
+          <Button
+            type="submit"
+            label="Sign in"
+            onClick={() => navigate('/home')}
+            variant="primary"
+            style={{ fontWeight: 800, fontSize: "24px" }}
+          />
+
+          <div className="divider">or</div>
+
+            <Button
+            label="Submit an application"
+            onClick={() => navigate('/home')}
+            variant="accent"
+            style={{ fontWeight: 800, fontSize: "24px" }}
+            />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
